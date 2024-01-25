36e25dcd6fdc7763cf0fdda4bd907adad5f1cd37
NachiketSaraf <sarafnachiket@gmail.com>
2024-01-25 17:57:55 +0530

    implementation of graph using matrix

---
+++ src/main/java/com/sde/sheet/graph/implementation/AdjacencyMatrix.java
@@ -0,0 +1,48 @@
+package com.sde.sheet.graph.implementation;
+
+public class AdjacencyMatrix {
+    public static void main(String[] args) {
+        int n = 5;
+        int m = 5;
+
+        int[][] adjMatrix = new int[n+1][m+1];
+
+        adjMatrix[1][2]=1;
+        adjMatrix[2][1]=1;
+
+        adjMatrix[2][4]=1;
+        adjMatrix[4][2]=1;
+
+        adjMatrix[1][3]=1;
+        adjMatrix[3][1]=1;
+
+        adjMatrix[3][4]=1;
+        adjMatrix[4][3]=1;
+
+        adjMatrix[2][5]=1;
+        adjMatrix[5][2]=1;
+
+        adjMatrix[4][5]=1;
+        adjMatrix[5][4]=1;
+
+        for (int i =0 ;i< adjMatrix.length;i++) System.out.print(i + " ");
+
+        System.out.println("\nnodes of graph ^");
+
+        for (int[] a:adjMatrix) {
+
+            for (int b:a) {
+                System.out.print(b + " ");
+            }
+
+            System.out.println();
+        }
+//        1----------------2 \
+//        |                |   \
+//        |                |    \
+//        |                |     5
+//        |                |    /
+//        |                |   /
+//        3----------------4 /
+    }
+}
